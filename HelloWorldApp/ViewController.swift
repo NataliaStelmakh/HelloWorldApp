e11855ab2b80d1197dec9b93c35c4d6b0f1150a2
Natali Stelmakh <stelmakh.natali@gmail.com>
2020-08-20 13:38:33 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Natali Stelmakh on 20.08.2020.
+//  Copyright © 2020 Natali Stelmakh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
